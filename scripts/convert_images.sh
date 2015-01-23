1bd3bcfb20fd86a4d85c73c90b20e9f60d89ce0e
Ubuntu <ubuntu@ip-172-31-8-120.eu-west-1.compute.internal>
2015-01-23 19:07:17 +0000

    Fix bug

--- scripts/convert_images.sh
+++ scripts/convert_images.sh
@@ -1,8 +1,6 @@
 # Convert images and pad intead of stretching the image
 new_name="data/256_padded"
 mkdir $new_name
-mkdir $new_name/train_all
-mkdir $new_name/test
 
 # Train images
 cp -R data/raw/train $new_name/train_all # Quick and dirty way to copy folders structure
@@ -15,7 +13,7 @@ done
 
 # Test images
 cp -R data/raw/test $new_name/test
-for name in $new_name/test *.jpg; do
+for name in $new_name/test/*.jpg; do
     convert -gravity center -extent 256x256 $name $name
 done
 

a800b99cfdeb403b4c606b79e473c0adc6853191
Ubuntu <ubuntu@ip-172-31-43-246.eu-west-1.compute.internal>
2015-03-01 21:33:11 +0000

    Latest changes for balanced calsses dataset

--- scripts/convert_images.sh
+++ scripts/convert_images.sh
@@ -3,15 +3,15 @@ new_name="data/64_sp"
 mkdir $new_name
 
 # Train images
-cp -R data/raw/train $new_name/train_all # Quick and dirty way to copy folders structure
-for d in $new_name/train_all/*; do
-    echo "Working on... $d"
-    for name in $d/*.jpg; do
+#cp -R data/raw/train $new_name/train_all # Quick and dirty way to copy folders structure
+#for d in $new_name/train_all/*; do
+#    echo "Working on... $d"
+#    for name in $d/*.jpg; do
         #convert -resize 256x256\! $name $name
         #convert -gravity center -extent 256x256 $name $name
-        convert -resize 64x64 -gravity center -extent 64x64 $name $name 
-    done
-done
+#        convert -resize 64x64 -gravity center -extent 64x64 $name $name 
+#    done
+#done
 
 # Test images
 cp -R data/raw/test $new_name/test
--- scripts/create_image_db.sh
+++ scripts/create_image_db.sh
@@ -3,14 +3,14 @@
 # N.B. set the path to the imagenet train + val data dirs
 
 EXAMPLE=.
-DATA=data/64_sp
+DATA=data/64_bal
 #DATA=data/256_scalepad
 #DATA=data/256_scalepad
 TOOLS=../../caffe/build/tools
 
-TRAIN_DATA_ROOT=data/64_sp/train/
-VAL_DATA_ROOT=data/64_sp/val/
-TEST_DATA_ROOT=data/64_sp/test/
+TRAIN_DATA_ROOT=data/64_bal/train/
+VAL_DATA_ROOT=data/64_bal/val/
+TEST_DATA_ROOT=data/64_bal/test/
 
 # Set RESIZE=true to resize the images to 256x256. Leave as false if images have
 # already been resized using another tool.
@@ -43,27 +43,27 @@ if [ ! -d "$TEST_DATA_ROOT" ]; then
 fi
 echo "Creating train lmdb..."
 
-GLOG_logtostderr=1 $TOOLS/convert_imageset \
-    --resize_height=$RESIZE_HEIGHT \
-    --resize_width=$RESIZE_WIDTH \
-    --shuffle \
-    --gray \
-    $TRAIN_DATA_ROOT \
-    $DATA/train.txt \
-    $DATA/train_lmdb
+#GLOG_logtostderr=1 $TOOLS/convert_imageset \
+#    --resize_height=$RESIZE_HEIGHT \
+#    --resize_width=$RESIZE_WIDTH \
+#    --shuffle \
+#    --gray \
+#    $TRAIN_DATA_ROOT \
+#    $DATA/train.txt \
+#    $DATA/train_lmdb
 
 echo "Creating val lmdb..."
 
-GLOG_logtostderr=1 $TOOLS/convert_imageset \
-    --resize_height=$RESIZE_HEIGHT \
-    --resize_width=$RESIZE_WIDTH \
-    --shuffle \
-    --gray \
-    $VAL_DATA_ROOT \
-    $DATA/val.txt \
-    $DATA/val_lmdb
+#GLOG_logtostderr=1 $TOOLS/convert_imageset \
+#    --resize_height=$RESIZE_HEIGHT \
+#    --resize_width=$RESIZE_WIDTH \
+#    --shuffle \
+#    --gray \
+#    $VAL_DATA_ROOT \
+#    $DATA/val.txt \
+#    $DATA/val_lmdb
 
-#echo "Creating test lmdb..."
+echo "Creating test lmdb..."
 
 GLOG_logtostderr=1 $TOOLS/convert_imageset \
     --resize_height=$RESIZE_HEIGHT \
